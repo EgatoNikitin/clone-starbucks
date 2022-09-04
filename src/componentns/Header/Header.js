bc14ab525e2c54ebcdd19d099b029251c591693a
EgatoNikitin <ve4er_top@mail.ru>
2022-09-04 14:08:31 +0300

    w

--- src/componentns/Header/Header.js
+++ src/componentns/Header/Header.js
@@ -5,37 +5,41 @@ import locationPin from '../../imgs/location.svg';
 import './Header.scss';
 import {Buttons} from '../Buttons/Buttons';
 
-export const Header = ()=>{
+export const Header = () => {
   return (
     <header className='header'>
-      <div className='header--right'>
-        <Link to='/'>
-          <img
-            src={starBucksIcon}
-            alt='starbucks icon'
-            className='header-icon'/>
-        </Link>
-        <nav className='header--navigation'>
-          <ul className='navigation--list'>
-            <li className='navigation--list--item'>menu</li>
-            <li className='navigation--list--item'>rewards</li>
-            <li className='navigation--list--item'>gift cards</li>
-          </ul>
-        </nav>
-      </div>
-      <div className='header--left'>
-        <div className='header--location'>
-          <img src={locationPin} alt='location-pin' className='location--pin'/>
-          <span className='location--text'>Find a store</span>
-        </div>
-        <Link to='/login'>
-          <Buttons type='light' text='Sign in'/>
-        </Link>
-        <Link to='/login'>
-          <Buttons type='black' text='Join now'/>
-        </Link>
-      </div>
+      <Link to='/'>
+        <img
+          src={starBucksIcon}
+          alt='starbucks icon'
+          className='header-icon' />
+      </Link>
+      <div className='header--wrapper'>
 
+        <div className='header--right'>
+          <nav className='header--navigation'>
+            <ul className='navigation--list'>
+              <li className='navigation--list--item'>menu</li>
+              <li className='navigation--list--item'>rewards</li>
+              <li className='navigation--list--item'>gift cards</li>
+            </ul>
+          </nav>
+        </div>
+        <div className='header--left'>
+          <div className='header--location'>
+            <img src={locationPin}
+              alt='location-pin'
+              className='location--pin' />
+            <span className='location--text'>Find a store</span>
+          </div>
+          <Link to='/login'>
+            <Buttons type='light' text='Sign in' />
+          </Link>
+          <Link to='/login'>
+            <Buttons type='black' text='Join now' />
+          </Link>
+        </div>
+      </div>
     </header>
   );
 };
