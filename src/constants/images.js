b9b1bbabe89c95cbd4cd0d0d3fc0dc6769ea9e46
EgatoNikitin <ve4er_top@mail.ru>
2022-09-16 19:50:10 +0300

    add imgs

--- src/constants/images.js
+++ src/constants/images.js
@@ -1,31 +1,113 @@
-import americano from '../imgs/menu/hot coffees/americano/americano.jpg';
+import americano from '../imgs/menu/Drinks/Hot Coffees/americano/americano.jpg';
 import caffeMisto
-  from '../imgs/menu/hot coffees/Brewed Coffees/caffe misto.jpg';
+  from '../imgs/menu/Drinks/Hot Coffees/Brewed Coffees/caffe misto.jpg';
 import roast
-  from '../imgs/menu/hot coffees/Brewed Coffees/roast.jpg';
+  from '../imgs/menu/Drinks/Hot Coffees/Brewed Coffees/roast - pike place.jpg';
 import darkRoast
-  from '../imgs/menu/hot coffees/Brewed Coffees/dark roast.jpg';
+  from '../imgs/menu/Drinks/Hot Coffees/Brewed Coffees/dark roast.jpg';
 import roastPike
-  from '../imgs/menu/hot coffees/Brewed Coffees/roast - pike place.jpg';
+  from '../imgs/menu/Drinks/Hot Coffees/Brewed Coffees/roast - pike place.jpg';
 import veranda
-  from '../imgs/menu/hot coffees/Brewed Coffees/veranda blend.jpg';
-import cappac from '../imgs/menu/hot coffees/Cappuccinos/IMG-5375.jpg';
-import espresso1 from '../imgs/menu/hot coffees/Espresso Shots/IMG-5376.jpg';
-import espresso2 from '../imgs/menu/hot coffees/Espresso Shots/IMG-5378.jpg';
-import flatWhites1 from '../imgs/menu/hot coffees/Flat Whites/IMG-5379.jpg';
-import flatWhites2 from '../imgs/menu/hot coffees/Flat Whites/IMG-5380.jpg';
-import lattes1 from '../imgs/menu/hot coffees/Lattes/IMG-5381.jpg';
-import lattes2 from '../imgs/menu/hot coffees/Lattes/IMG-5382.jpg';
-import lattes3 from '../imgs/menu/hot coffees/Lattes/IMG-5383.jpg';
-import lattes5 from '../imgs/menu/hot coffees/Lattes/IMG-5384.jpg';
-import lattes6 from '../imgs/menu/hot coffees/Lattes/IMG-5385.jpg';
-import lattes4 from '../imgs/menu/hot coffees/Lattes/IMG-5386.jpg';
-import macchitos1 from '../imgs/menu/hot coffees/Macchiatos/IMG-5387.jpg';
-import macchitos2 from '../imgs/menu/hot coffees/Macchiatos/IMG-5388.jpg';
-import mochas1 from '../imgs/menu/hot coffees/Mochas/IMG-5391.jpg';
-import mochas2 from '../imgs/menu/hot coffees/Mochas/IMG-5390.jpg';
-import mochas3 from '../imgs/menu/hot coffees/Mochas/IMG-5389.jpg';
-
+  from '../imgs/menu/Drinks/Hot Coffees/Brewed Coffees/veranda blend.jpg';
+import cappac from '../imgs/menu/Drinks/Hot Coffees/Cappuccinos/IMG-5375.jpg';
+import espresso1
+  from '../imgs/menu/Drinks/Hot Coffees/Espresso Shots/IMG-5376.jpg';
+import espresso2
+  from '../imgs/menu/Drinks/Hot Coffees/Espresso Shots/IMG-5378.jpg';
+import flatWhites1
+  from '../imgs/menu/Drinks/Hot Coffees/Flat Whites/IMG-5379.jpg';
+import flatWhites2
+  from '../imgs/menu/Drinks/Hot Coffees/Flat Whites/IMG-5380.jpg';
+import lattes1 from '../imgs/menu/Drinks/Hot Coffees/Lattes//IMG-5381.jpg';
+import lattes2 from '../imgs/menu/Drinks/Hot Coffees/Lattes/IMG-5382.jpg';
+import lattes3 from '../imgs/menu/Drinks/Hot Coffees/Lattes/IMG-5383.jpg';
+import lattes5 from '../imgs/menu/Drinks/Hot Coffees/Lattes/IMG-5384.jpg';
+import lattes6 from '../imgs/menu/Drinks/Hot Coffees/Lattes/IMG-5385.jpg';
+import lattes4 from '../imgs/menu/Drinks/Hot Coffees/Lattes/IMG-5386.jpg';
+import macchitos1
+  from '../imgs/menu/Drinks/Hot Coffees/Macchiatos/IMG-5387.jpg';
+import macchitos2
+  from '../imgs/menu/Drinks/Hot Coffees/Macchiatos/IMG-5388.jpg';
+import mochas1 from '../imgs/menu/Drinks/Hot Coffees/Mochas/IMG-5391.jpg';
+import mochas2 from '../imgs/menu/Drinks/Hot Coffees/Mochas/IMG-5390.jpg';
+import mochas3 from '../imgs/menu/Drinks/Hot Coffees/Mochas/IMG-5389.jpg';
+import clover1 from
+  '../imgs/menu/Drinks/Cold Coffees/Iced Clover Brewed Coffees/IMG-5946.jpg';
+import clover2 from
+  '../imgs/menu/Drinks/Cold Coffees/Iced Clover Brewed Coffees/IMG-5947.jpg';
+import clover3
+  from
+  '../imgs/menu/Drinks/Cold Coffees/Iced Clover Brewed Coffees/IMG-5948.jpg';
+import clover4
+  from
+  '../imgs/menu/Drinks/Cold Coffees/Iced Clover Brewed Coffees/IMG-5949.jpg';
+import clover5
+  from
+  '../imgs/menu/Drinks/Cold Coffees/Iced Clover Brewed Coffees/IMG-5950.jpg';
+import clover6
+  from
+  '../imgs/menu/Drinks/Cold Coffees/Iced Clover Brewed Coffees/IMG-5951.jpg';
+import clover7
+  from
+  '../imgs/menu/Drinks/Cold Coffees/Iced Clover Brewed Coffees/IMG-5952.jpg';
+import clover8
+  from
+  '../imgs/menu/Drinks/Cold Coffees/Iced Clover Brewed Coffees/IMG-5953.jpg';
+import clover9
+  from
+  '../imgs/menu/Drinks/Cold Coffees/Iced Clover Brewed Coffees/IMG-5954.jpg';
+import clover10
+  from
+  '../imgs/menu/Drinks/Cold Coffees/Iced Clover Brewed Coffees/IMG-5955.jpg';
+import clover11
+  from
+  '../imgs/menu/Drinks/Cold Coffees/Iced Clover Brewed Coffees/IMG-5956.jpg';
+import clover12
+  from
+  '../imgs/menu/Drinks/Cold Coffees/Iced Clover Brewed Coffees/IMG-5957.jpg';
+import clover13
+  from
+  '../imgs/menu/Drinks/Cold Coffees/Iced Clover Brewed Coffees/IMG-5958.jpg';
+import clover14
+  from
+  '../imgs/menu/Drinks/Cold Coffees/Iced Clover Brewed Coffees/IMG-5959.jpg';
+import clover15
+  from
+  '../imgs/menu/Drinks/Cold Coffees/Iced Clover Brewed Coffees/IMG-5960.jpg';
+import clover16
+  from
+  '../imgs/menu/Drinks/Cold Coffees/Iced Clover Brewed Coffees/IMG-5961.jpg';
+import clover17
+  from
+  '../imgs/menu/Drinks/Cold Coffees/Iced Clover Brewed Coffees/IMG-5962.jpg';
+import clover18
+  from
+  '../imgs/menu/Drinks/Cold Coffees/Iced Clover Brewed Coffees/IMG-5963.jpg';
+import clover19
+  from
+  '../imgs/menu/Drinks/Cold Coffees/Iced Clover Brewed Coffees/IMG-5964.jpg';
+import clover20
+  from
+  '../imgs/menu/Drinks/Cold Coffees/Iced Clover Brewed Coffees/IMG-5965.jpg';
+import clover21
+  from
+  '../imgs/menu/Drinks/Cold Coffees/Iced Clover Brewed Coffees/IMG-5966.jpg';
+import clover22
+  from
+  '../imgs/menu/Drinks/Cold Coffees/Iced Clover Brewed Coffees/IMG-5967.jpg';
+import coffeeTravelers from '../imgs/menu/Coffee Travelers.png';
+import chaiTeas1 from '../imgs/menu/Drinks/Hot Teas/Chai Teas/IMG-5392.jpg';
+import chaiTeas2 from '../imgs/menu/Drinks/Hot Teas/Chai Teas/IMG-5393.jpg';
+import blackTeas1 from '../imgs/menu/Drinks/Hot Teas/Black Teas/IMG-5394.jpg';
+import blackTeas2 from '../imgs/menu/Drinks/Hot Teas/Black Teas/IMG-5395.jpg';
+import blackTeas3 from '../imgs/menu/Drinks/Hot Teas/Black Teas/IMG-5398.jpg';
+import blackTeas4 from '../imgs/menu/Drinks/Hot Teas/Black Teas/IMG-5399.jpg';
+import greenTeas1 from '../imgs/menu/Drinks/Hot Teas/Green Teas/IMG-5400.jpg';
+import greenTeas2 from '../imgs/menu/Drinks/Hot Teas/Green Teas/IMG-5401.jpg';
+import greenTeas3 from '../imgs/menu/Drinks/Hot Teas/Green Teas/IMG-5402.jpg';
+import greenTeas4 from '../imgs/menu/Drinks/Hot Teas/Green Teas/IMG-5404.jpg';
+import herbalTeas1 from '../imgs/menu/Drinks/Hot Teas/Herbal Teas/IMG-5405.jpg';
+import herbalTeas2 from '../imgs/menu/Drinks/Hot Teas/Herbal Teas/IMG-5406.jpg';
 
 
 export const BREWED_COFFEE = {
@@ -73,4 +155,50 @@ export const MOCHAS = {
   mochas2,
   mochas3,
 };
-
+export const CLOVER = {
+  clover1,
+  clover2,
+  clover3,
+  clover4,
+  clover5,
+  clover6,
+  clover7,
+  clover8,
+  clover9,
+  clover10,
+  clover11,
+  clover12,
+  clover13,
+  clover14,
+  clover15,
+  clover16,
+  clover17,
+  clover18,
+  clover19,
+  clover20,
+  clover21,
+  clover22,
+};
+export const TRAVELERS = {
+  coffeeTravelers,
+};
+export const CHAITEAS = {
+  chaiTeas1,
+  chaiTeas2,
+};
+export const BLACKTEAS = {
+  blackTeas1,
+  blackTeas2,
+  blackTeas3,
+  blackTeas4,
+};
+export const GREENTEAS = {
+  greenTeas1,
+  greenTeas2,
+  greenTeas3,
+  greenTeas4,
+};
+export const HERBALTEAS = {
+  herbalTeas1,
+  herbalTeas2,
+};
