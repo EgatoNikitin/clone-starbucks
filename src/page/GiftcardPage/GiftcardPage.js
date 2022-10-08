a75cda60004cf9fa848fc15a09cc2bb7355c9537
alinapodrez <alinapodrez@gmail.com>
2022-10-08 15:08:26 +0300

    add slick

--- src/page/GiftcardPage/GiftcardPage.js
+++ src/page/GiftcardPage/GiftcardPage.js
@@ -1,14 +1,34 @@
 import {Header} from '../../componentns/Header/Header';
 import './GiftcardPage.scss';
 import {Footer} from '../../componentns/Footer/Footer';
-import {Slider} from '../../componentns/Slider/Slider';
+import Slider from 'react-slick';
+import img from '../../imgs/main-content/apple.jpg';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
+
 
 const GiftcardPage = () => {
+  const settings = {
+    dots: false,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 5,
+    slidesToScroll: 1,
+  };
+
   return (
     <div className='main--wrapper'>
       <Header />
+      <Slider {...settings}>
+        <img src={img} alt='behjw' />
+        <img src={img} alt='behjw' />
+        <img src={img} alt='behjw' />
+        <img src={img} alt='behjw' />
+        <img src={img} alt='behjw' />
+        <img src={img} alt='behjw' />
+        <img src={img} alt='behjw' />
+      </Slider>
       <Footer />
-      <Slider/>
     </div>
   );
 };
