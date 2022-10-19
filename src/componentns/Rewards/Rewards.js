f075259aeb614d18b32c01f6582cf67de4e4e5bf
alinapodrez <alinapodrez@gmail.com>
2022-10-20 00:08:24 +0300

    rewars add section

--- src/componentns/Rewards/Rewards.js
+++ src/componentns/Rewards/Rewards.js
@@ -31,6 +31,35 @@ export const Rewards = ()=>{
         <p>Or <a>join in the app</a> for the best experience</p>
       </section>
 
+      <section className='rewards--easy-started'>
+        <div className='easy-started-title'>
+          <h2>Getting started is easy</h2>
+          <p>Earn Stars and get rewarded in a few easy steps.</p>
+        </div>
+        <div className='easy-started-steps'>
+          <div className='easy-started-item'>
+            <span>1</span>
+            <h3>Create an account</h3>
+            <p>To get started, join now. You can also join in the app 
+              to get access to the full range of Starbucks® 
+              Rewards benefits.</p>
+          </div>
+          <div className='easy-started-item'>
+            <span>2</span>
+            <h3>Order and pay how you’d like</h3>
+            <p>Use cash, credit/debit card or save some time and pay right 
+              through the app. You’ll collect Stars all ways. Learn how</p>
+          </div>
+          <div className='easy-started-item'>
+            <span>3</span>
+            <h3>Earn Stars, get Rewards</h3>
+            <p>As you earn Stars, you can redeem them for Rewards—like 
+              free food, drinks, and more. Start redeeming 
+              with as little as 25 Stars!</p>
+          </div>
+        </div>
+      </section>
+
       <div className="stars--wrapper">
         <h2>Get your favorites for free</h2>
         <ul className="stars--list"
